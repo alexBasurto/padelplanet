98b2b3a5e4d4ca78397174d036765b53af41c7fd
Alex Basurto <alexbasurto1@gmail.com>
2023-11-15 21:22:55 +0100

    Estructura. courtsController.js y clubsController.js

---
+++ src/controllers/clubsController.js
@@ -0,0 +1,98 @@
+import { Op } from "sequelize";
+
+import clubsModel from "../models/clubsModel.js";
+import courtsModel from "../models/courtsModel.js";
+
+const getAll = async (q = null) => {
+    const options = {};
+    if (q) {
+        options.where = {
+            clubName: { [Op.like]: `%${q}%` },
+        };
+    }
+    try {
+        const clubs = await clubsModel.findAll(options);
+        return [null, clubs];
+    } catch (e) {
+        return [e.message, null];
+    }
+};
+
+
+const getById = async (id) => {
+    const options = {};
+    try {
+        const club = await clubsModel.findByPk(id, options);
+        return [null, club];
+    } catch (e) {
+        return [e.message, null];
+    }
+};
+
+const create = async (clubName, address, comments, photo) => {
+    if (clubName === undefined || address === undefined) {
+        const error = "Rellene todos los campos obligatorios.";
+        return [error, null];
+    }
+    try {
+        const club = await clubsModel.create({
+            clubName, address, comments, photo
+        });
+        return [null, club];
+    } catch (e) {
+        return [e.message, null];
+    }
+};
+
+const update = async (idClub, clubName, address, comments, photo) => {
+    if (idClub == undefined) {
+        const error = "Tienes que especificar un ID válido";
+        return [error, null];
+    }
+    if (clubName === undefined || address === undefined) {
+        const error =
+            "Los campos nombre y domicilio son obligatorios.";
+        return [error, null];
+    }
+    try {
+        const club = await clubsModel.findByPk(idClub);
+        club.clubName = clubName;
+        club.address = address;
+        club.comments = comments;
+        club.photo = photo;
+        club.save();
+        return [null, club];
+    } catch (e) {
+        console.log(e);
+        return [e.message, null];
+    }
+};
+
+const remove = async (idClub) => {
+    try {
+        const club = await clubsModel.findByPk(idClub);
+        if (!club) {
+            const error = "No se ha encontrado ningún club con ese ID.";
+            return [error, null];
+        }
+        const checkCourts = await courtsModel.count({
+            where: { idclub: club.idclub },
+        });
+        if (checkCourts > 0) {
+            const error = `No se puede borrar. Existe(n) ${checkCourts} pistas(s) de este club. Borre las pistas primero.`;
+            return [error, null];
+        }
+        club.destroy();
+        return [null, club];
+    } catch (e) {
+        return [e.message, null];
+    }
+};
+
+export default {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
---
+++ src/controllers/courtsController.js
@@ -0,0 +1,104 @@
+import { Op } from "sequelize";
+
+import courtsModel from "../models/courtsModel.js";
+import clubsModel from "../models/clubsModel.js";
+import gamesModel from "../models/gamesModel.js";
+
+const getAll = async (idClub = null) => {
+    const options = {};
+    if (idClub) {
+        options.where = {
+            idClub: {[Op.eq]: idClub}
+        };
+    }
+    try {
+        const courts = await courtsModel.findAll(options);
+        return [null, courts];
+    } catch (e) {
+        return [e.message, null];
+    }
+};
+
+
+const getById = async (idCourt) => {
+    const options = {};
+    try {
+        const court = await courtsModel.findByPk(idCourt, options);
+        return [null, court];
+    } catch (e) {
+        return [e.message, null];
+    }
+};
+
+const create = async (idClub, courtName, courtType, wallType, widthType) => {
+    if (idClub === undefined || courtName === undefined || courtType === undefined ||  wallType === undefined ||  widthType === undefined) {
+        const error = "Rellene todos los campos.";
+        return [error, null];
+    }
+    try {
+        const court = await courtsModel.create({
+            idClub, courtName, courtType, wallType, widthType
+        });
+        return [null, court];
+    } catch (e) {
+        return [e.message, null];
+    }
+};
+
+const update = async (idCourt, idClub, courtName, courtType, wallType, widthType) => {
+    if (idCourt == undefined) {
+        const error = "Tienes que especificar un ID válido";
+        return [error, null];
+    }
+    if (idClub === undefined
+        || courtName === undefined
+        || courtType === undefined
+        || wallType === undefined
+        || widthType === undefined) {
+        const error =
+            "Todos los campos son obligatorios.";
+        return [error, null];
+    }
+    try {
+        const court = await courtsModel.findByPk(idCourt);
+        court.idClub = idClub;
+        court.courtName = courtName;
+        court.courtType = courtType;
+        court.wallType = wallType;
+        court.widthType = widthType;
+        court.save();
+        return [null, court];
+    } catch (e) {
+        console.log(e);
+        return [e.message, null];
+    }
+};
+
+const remove = async (idCourt) => {
+    try {
+        const court = await courtsModel.findByPk(idCourt);
+        if (!court) {
+            const error = "No se ha encontrado ninguna pista con ese ID.";
+            return [error, null];
+        }
+        const checkGames = await gamesModel.count({
+            where: { idCourt: court.idCourt },
+        });
+        if (checkGames > 0) {
+            const error = `No se puede borrar. Existe(n) ${checkGames} partido(s) en el histórico de esta pista.`;
+            return [error, null];
+        }
+        court.destroy();
+        return [null, court];
+    } catch (e) {
+        return [e.message, null];
+    }
+};
+
+export default {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
